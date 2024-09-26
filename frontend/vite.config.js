44207ac91e4504e695d3a30c3bb0327d526763b6
GauravSaini144 <149235914+GauravSaini144@users.noreply.github.com>
2024-09-26 17:51:56 +0530

    eleventh commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-   new NodePolyfillPlugin()
+
   ],
 
-  resolve: {
-    alias: {
-      path: 'path-browserify',
-      os: 'os-browserify/browser',
-      crypto: 'crypto-browserify'
-    }
-  }
+ 
 })
