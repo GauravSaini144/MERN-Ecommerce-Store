65d103ef21266a82ff1f51dc0b7edfd020833112
GauravSaini144 <149235914+GauravSaini144@users.noreply.github.com>
2024-09-26 16:59:23 +0530

    Sixth Commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
       '@mui/icons-material': '@mui/icons-material/esm',
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: ['path', 'os', 'crypto'],
+    },
+  },
+});
+
